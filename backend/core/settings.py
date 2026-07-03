75047a825642d8b692b41d88eae5cfa50b556779
Felipe Bustos <felipe.bustosnav@gmail.com>
2026-07-03 15:57:59 -0400

    Fix syntax error in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -71,11 +71,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
